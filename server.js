076ce6cf3917c6d93ba52052a7151936b3a3a91c
Deep Jadav <jadavdeep560@gmail.com>
2026-08-24 22:07:24 +0530

    Initial commit

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-
+const cors = require('cors')
 require('dotenv').config();
 const reportRouter = require('./routes/reportrouter');
 const app = express();
 
 const PORT = process.env.PORT || 3000;      
-
+app.use(cors())
 app.use(express.json());
 app.get('/',(req,res)=>{
     res.json({message:"API is Live"})
